54853f8b35ccde96a44689bdf5393c0886f7986c
Marwan Ali <marwanali2428@gmail.com>
2017-02-14 11:44:59 -0500

    Update UltimateBoard.java

--- UltimateBoard.java
+++ UltimateBoard.java
@@ -2,12 +2,50 @@ import java.util.*;
 
 public class UltimateBoard
 {
-	public UltimateBoard();
+	// Player one
+	// Player two
 
-	public String toString();
+	// Nine LocalBoard components of the ultimate board.
+	private LocalBoard boardArr[][];
 
-// 	public static void main(String[] args)
-// 	{
-// 		UltimateBoard testBoard = new UltimateBoard();
-// 	}
+	public UltimateBoard()
+	{
+		this.boardArr = new LocalBoard[3][3];
+		for (int i = 0; i < boardArr.length; i++)
+		{
+			for (int j = 0; i < boardArr[i].length; j++)
+			{
+				this.boardArr[i][j] = new LocalBoard(i*j);
+			}
+		}
+	}
+
+	/**
+	* Change turns between players after a player plays.
+	*/
+	public boolean setTurn();
+
+	/**
+  * Returns a string representation of the board.
+  */
+	public String toString()
+	{
+		StringBuilder ultimateStr = new StringBuilder();
+		for (int i = 0; i < boardArr.length; i++)
+		{
+			for (int j = 0; i < boardArr[i].length; j++)
+			{
+				String localStr = boardArr[i][j].toString();
+				ultimateStr.append(localStr + "  ");
+			}
+			ultimateStr.append("\n\n");
+		}
+
+    return ultimateStr.toString();
+	}
+
+	// public static void main(String[] args)
+  // {
+  //   UltimateBoard testBoard = new UltimateBoard();
+  // }
 }
