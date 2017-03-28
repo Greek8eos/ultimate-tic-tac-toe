60d26cbda5800b9d7b79531e90353ce34062d6f6
Marwan Ali <marwanali2428@gmail.com>
2017-03-28 13:26:49 -0400

    Update UltimateBoard.java

--- UltimateBoard.java
+++ UltimateBoard.java
@@ -5,6 +5,8 @@ public class UltimateBoard
 
 	// Nine LocalBoard components of the ultimate board.
 	private LocalBoard boardArr[][];
+	private Player playOne;
+  	private Player playTwo;
 
 	/**
 	* Ultimate Board constructor
@@ -13,12 +15,15 @@ public class UltimateBoard
 	public UltimateBoard()
 	{
 		this.boardArr = new LocalBoard[3][3];
+		this.playOne = new Player('X');
+		this.playTwo = new Player('O');
+		
 		int count = 0;
 		for (int i = 0; i < boardArr.length; i++)
 		{
 			for (int j = 0; i < boardArr[i].length; j++)
 			{
-				this.boardArr[i][j] = new LocalBoard(count);
+				this.boardArr[i][j] = new LocalBoard(count, playOne, playTwo);
 				count++;
 			}
 		}
