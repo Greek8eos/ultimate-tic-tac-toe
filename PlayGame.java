5d2db2918bb424b70e058e270ee3eea2539273ca
EthanTPainter <ethantpainter@gmail.com>
2017-04-04 12:23:02 -0400

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -2,5 +2,19 @@ import java.util.*;
 
 public class PlayGame
 {
-
+  //Declare and Initialize variables
+  Player one = new Player('X');
+  Player two = new Player('O');
+  UltimateBoard ub = new UltimateBoard();
+  
+  
+  //Driver for Playing the Game
+  public static void main(String[] args)
+  {
+    //Choose gamemodes, Play games, etc.
+    
+    
+  }
+  
+  
 }
