b73e08cae95cbbb430366664fc0113bbfc2ae2b7
Greek8eos <kountourisg@yahoo.com>
2017-04-10 10:41:13 -0400

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -5,6 +5,7 @@ public class PlayGame
   //Declare and Initialize variables
   Player one = new Player('X');
   Player two = new Player('O');
+  LocalBoard lb= new LocalBoard();
   UltimateBoard ub = new UltimateBoard();
   
   
