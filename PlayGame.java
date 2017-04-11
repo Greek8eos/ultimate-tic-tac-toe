9487f32b68ebd210e32c27f8c5d5970a24f7fe66
EthanTPainter <ethantpainter@gmail.com>
2017-04-11 12:32:22 -0400

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -6,7 +6,8 @@ public class PlayGame
   Player one;
   Player two;
   UltimateBoard gameBoard;
-  
+  boolean newGame;
+  int chosenMode;
   
   //Driver for Playing the Game
   public static void main(String[] args)
@@ -15,6 +16,39 @@ public class PlayGame
     this.one = new Player('X'); 
     this.two = new Player('O');
     this.ub = new UltimateBoard();
+    
+    //Start new Game
+    System.out.println("Start a new game? (Y/N)");
+    
+    //if no, exit();
+    
+    //Get new gamemode
+    System.out.println("Gamemodes: \n 1) Winner when one local board is won \n 2) Winner when three local boards are won in a row");
+    
+    
+    //Play Game
+    System.out.println("Start playing!");
+    
+    //While game mode is not won
+    while()
+    {
+      switch(chosenMode){
+        case 1:
+          //When a local board is won
+          
+          break;
+        case 2:
+          //When three local boards are won in a row
+          
+          break;
+        default:
+          System.out.println("ERROR: GameMode not recognized. Enter new gamemode information: ");
+          //Read in new game mode input to follow game mode 1 or 2
+          
+          break;
+      }
+    }
+    
     
   }
 }
