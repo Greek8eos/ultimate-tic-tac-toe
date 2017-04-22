ab04eb4e003d35adb2cf21a22f45ddc6c7501c6f
EthanTPainter <ethantpainter@gmail.com>
2017-04-22 16:17:24 -0400

    Update PlayGame.java

--- PlayGame.java
+++ PlayGame.java
@@ -12,94 +12,109 @@ public class PlayGame
   
   
   //Driver for Playing the Game
-  public static void main(String[] args)
-  throws IOException
+  public static void main(String[] args ) throws IOException
   {
     //Choose gamemodes, Play games, etc.
     gameBoard = new UltimateBoard();
     
-    	String line;
-    	try 
-    	{
-    		InputStream fis = new FileInputStream("/home/eadom/communication.txt");
-    		InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-    		BufferedReader br = new BufferedReader(isr);
-    	} 
-    	while ((line = br.readLine()) == null) 
-    	{
-    		//just waiting for option input
-    	}
-    	
-    	String[] command = line.split("\\s+");
-    	int chosenMode = Integer.parseInt(command[1]); 
-    	switch(chosenMode)
-    	{
-    		/*Player One begins game, with turn equals true
-      		Allow Player one to pick any spot on UltimateBoard
-      		The position chosen dictates the next local board to be used
-      		Whenever a local board is won, the game ends
-    		 */
-    	case 1:
+    String line;
+    InputStream fis = new FileInputStream("/home/eadom/communication.txt");
+    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+    BufferedReader br = new BufferedReader(isr);
+    
+    while ((line = br.readLine()) == null) {
+    //just waiting for option input
+    }
+    String[] command = line.split("\\s+");
+    int chosenMode = Integer.parseInt(command[1]); 
+
+    switch(chosenMode)
+    {
+      /*Player One begins game, with turn equals true
+      Allow Player one to pick any spot on UltimateBoard
+      The position chosen dictates the next local board to be used
+      Whenever a local board is won, the game ends
+      */
+      case 1:
         
-    		//Player One picks a position (A local board and local board position)
-    		//How is this represented? Are we receiving a string or input from the click in the UI
-    		//Player Two picks a position in a local board dictated by the previous position
-    		//This should continue in a loop until one local board is won
-    		int localBoardNumber = 0;
+        //Player One picks a position (A local board and local board position)
+        //How is this represented? Are we receiving a string or input from the click in the UI
+        //Player Two picks a position in a local board dictated by the previous position
+        //This should continue in a loop until one local board is won
+        int localBoardNumber = 0;
 
-    		// Added  by Eadom
-    		// String line;
-    		// try (
-    		// InputStream fis = new FileInputStream("/home/eadom/communication.txt");
-    		// InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
-    		// BufferedReader br = new BufferedReader(isr);
-    		// ) 
-    		// {
-    		// while ((line = br.readLine()) == null) {
-    		// //just waiting for option input
-    		// }
-    		// String[] command = line.split("\\s+")
-    		// int row = Integer.parseInt(command[1])
-    		// int col = Integer.parseInt(command[2])
-    		/////////////////////////////////////////
 
-    		gameBoard.playOne.setPlayerTurn();
-    		while(!gameBoard.getBoardArr(localBoardNumber).checkWon())
-    		{
-    			//  receive player input
-    			// lnum= local board number from python;
-
-    			if(gameBoard.getBoardArr(localBoardNumber).isSpotEmpty(row, col))
-    			{
-    				int[] nextLBArr = gameBoard.getBoardArr(localBoardNumber).setSpot(row,col))
+        gameBoard.getPlayerOne().setPlayerTurn();
+        while(!gameBoard.getBoardArr(localBoardNumber).isWon())
+        {
+        	// Added  by Eadom
+            InputStream fis1 = new FileInputStream("/home/eadom/communication.txt");
+            InputStreamReader isr1 = new InputStreamReader(fis1, Charset.forName("UTF-8"));
+            BufferedReader br1 = new BufferedReader(isr1);
+            
+            while ((line = br1.readLine()) == null) 
+            {
+            //just waiting for option input
+            }
+            String[] command1 = line.split("\\s+");
+            int row = Integer.parseInt(command[0]);
+            int col = Integer.parseInt(command[1]);
+            localBoardNumber=Integer.parseInt(command[2]);
+            		
+          if(gameBoard.getBoardArr(localBoardNumber).isSpotEmpty(row, col))
+          {
+            int[] nextLBArr = gameBoard.getBoardArr(localBoardNumber).setSpot(row,col);
+            gameBoard.getBoardArr(localBoardNumber).checkWon();
        
-            	if(gameBoard.getBoardArr(localBoardNumber).checkWon())
-            	{
-            		//send to python playerOne won
-            		//receive whether NewGame or Quit
-               
-            		//Check newG if it equals 'Y' or any other answer
+            if(gameBoard.getBoardArr(localBoardNumber).isWon())
+            {
+               //send to python playerOne won
+            	 try 
+                 {
+                   BufferedWriter bw = null;
+                   FileWriter fw = null;
+                   int playerNumber;
+                   if(gameBoard.getPlayerOne().getTurn())
+                   {
+                	   playerNumber=1;
+                   }
+                   else
+                   {
+                	   playerNumber=2;
+                   }
+                   String content = "Won " + playerNumber;
+
+                   fw = new FileWriter(FILENAME);
+                   bw = new BufferedWriter(fw);
+                   bw.write(content);
+
+                   System.out.println("Sending random command in a single line");
+                   if (bw != null)
+                     bw.close();
+
+                   if (fw != null)
+                     fw.close();
+
+                 }
+                 catch (IOException e) 
+                 {
+                   e.printStackTrace();
+                 }
              
-            		if(newGame)
-            		{
-            			// If input is 'Y' for "Yes" than this results in a new game
-            			//  main();
-            			//  exit();
-            			return;
-            		}
-            		else if
-            		{
-            			System.exit(0);
-            		}
+                 System.exit(0);
+              
+            }
+              else
+              {
+            	localBoardNumber = 3 * nextLBArr[0] + nextLBArr[1];
+            	gameBoard.getPlayerOne().switchTurn();
+            	gameBoard.getPlayerTwo().switchTurn();
+                try 
+                {
+                  BufferedWriter bw = null;
+                  FileWriter fw = null;
 
-            		else
-            		{
-            			try 
-            			{
-            				BufferedWriter bw = null;
-            				FileWriter fw = null;
-
-                  String content = "Feedback True Localboard 1";
+                  String content = "Feedback True Localboard "+localBoardNumber;
 
                   fw = new FileWriter(FILENAME);
                   bw = new BufferedWriter(fw);
@@ -118,13 +133,35 @@ public class PlayGame
                   e.printStackTrace();
                 }
 
-              // Eadom what is this line and where is it supposed to go.
-              // }, 0, 1000L, TimeUnit.MILLISECONDS);
-              //   }
-              // }
-
-              int nextLBnum = 3 * nextLBArr[0] + nextLBArr[1];
+              
             }
+          }
+          else
+          {
+        	  try 
+              {
+                BufferedWriter bw = null;
+                FileWriter fw = null;
+
+                String content = "Feedback False";
+
+                fw = new FileWriter(FILENAME);
+                bw = new BufferedWriter(fw);
+                bw.write(content);
+
+                System.out.println("Sending random command in a single line");
+                if (bw != null)
+                  bw.close();
+
+                if (fw != null)
+                  fw.close();
+
+              }
+              catch (IOException e) 
+              {
+                e.printStackTrace();
+              }
+
           }
         }
         
@@ -134,7 +171,168 @@ public class PlayGame
         //Player On picks a position (A local board and local board position)
         //Player Two picks a position in a local board dictated by the previous position
         //This should continue in a loop until three local boards are won in a row
-        
+    	  int localBoardNumber1 = 0;
+
+
+          gameBoard.getPlayerOne().setPlayerTurn();
+          while(!gameBoard.isWon())
+          {
+          	// Added  by Eadom
+              InputStream fis1 = new FileInputStream("/home/eadom/communication.txt");
+              InputStreamReader isr1 = new InputStreamReader(fis1, Charset.forName("UTF-8"));
+              BufferedReader br1 = new BufferedReader(isr1);
+              
+              while ((line = br1.readLine()) == null) 
+              {
+              //just waiting for option input
+              }
+              String[] command1 = line.split("\\s+");
+              int row = Integer.parseInt(command[0]);
+              int col = Integer.parseInt(command[1]);
+              localBoardNumber1=Integer.parseInt(command[2]);
+              		
+            if(gameBoard.getBoardArr(localBoardNumber1).isSpotEmpty(row, col))
+            {
+              int[] nextLBArr = gameBoard.getBoardArr(localBoardNumber1).setSpot(row,col);
+         
+              if(gameBoard.isWon())
+              {
+                 //send to python playerOne won
+              	 try 
+                   {
+                     BufferedWriter bw = null;
+                     FileWriter fw = null;
+                     int playerNumber;
+                     if(gameBoard.getPlayerOne().getTurn())
+                     {
+                  	   playerNumber=1;
+                     }
+                     else
+                     {
+                  	   playerNumber=2;
+                     }
+                     String content = "Won " + playerNumber;
+
+                     fw = new FileWriter(FILENAME);
+                     bw = new BufferedWriter(fw);
+                     bw.write(content);
+
+                     System.out.println("Sending random command in a single line");
+                     if (bw != null)
+                       bw.close();
+
+                     if (fw != null)
+                       fw.close();
+
+                   }
+                   catch (IOException e) 
+                   {
+                     e.printStackTrace();
+                   }
+               
+                   System.exit(0);
+                
+              }
+                else
+                {
+                	 if(gameBoard.getBoardArr(localBoardNumber).isWon())
+                     {
+                        //send to python playerOne won
+                     	 try 
+                          {
+                     		localBoardNumber = 3 * nextLBArr[0] + nextLBArr[1];
+                     		BufferedWriter bw = null;
+                            FileWriter fw = null;
+                            int playerNumber;
+                            if(gameBoard.getPlayerOne().getTurn())
+                            {
+                         	   playerNumber=1;
+                            }
+                            else
+                            {
+                         	   playerNumber=2;
+                            }
+                            String content = "LocalWon " + playerNumber + " " + localBoardNumber;
+
+                            fw = new FileWriter(FILENAME);
+                            bw = new BufferedWriter(fw);
+                            bw.write(content);
+
+                            System.out.println("Sending random command in a single line");
+                            if (bw != null)
+                              bw.close();
+
+                            if (fw != null)
+                              fw.close();
+
+                          }
+                          catch (IOException e) 
+                          {
+                            e.printStackTrace();
+                          }
+                     }
+                	 else
+                	 {
+                		 localBoardNumber = 3 * nextLBArr[0] + nextLBArr[1];
+                		 gameBoard.getPlayerOne().switchTurn();
+                		 gameBoard.getPlayerTwo().switchTurn();
+                		 try 
+                		 {
+                			 BufferedWriter bw = null;
+                			 FileWriter fw = null;
+
+                			 String content = "Feedback True Localboard "+localBoardNumber;
+
+                			 fw = new FileWriter(FILENAME);
+                			 bw = new BufferedWriter(fw);
+                			 bw.write(content);
+                    
+                			 System.out.println("Sending random command in a single line");
+                			 if (bw != null)
+                				 bw.close();
+
+                			 if (fw != null)
+                				 fw.close();
+
+                		 }
+                		 catch (IOException e) 
+                		 {
+                			 e.printStackTrace();
+                		 }
+
+                
+                	 }
+                }
+            }
+            else
+            {
+          	  try 
+                {
+                  BufferedWriter bw = null;
+                  FileWriter fw = null;
+
+                  String content = "Feedback False";
+
+                  fw = new FileWriter(FILENAME);
+                  bw = new BufferedWriter(fw);
+                  bw.write(content);
+
+                  System.out.println("Sending random command in a single line");
+                  if (bw != null)
+                    bw.close();
+
+                  if (fw != null)
+                    fw.close();
+
+                }
+                catch (IOException e) 
+                {
+                  e.printStackTrace();
+                }
+
+            }
+          }
+          
         
         break;
       default:
@@ -143,6 +341,7 @@ public class PlayGame
           System.out.println("Error: Chosen mode number not recognized. Enter new input:");
           chosenMode = sc.nextInt();
         }
-       }  
+        
     }
   }
+}
